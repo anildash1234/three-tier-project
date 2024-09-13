5ee5db4a392cec113be0666b669880a7a7e1c7c0
anildash1234 <168274843+anildash1234@users.noreply.github.com>
2024-09-13 13:55:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ provider "aws" {
  # data source 
  data "aws_vpc" "main" {
   tags = {
-    Name = "jump-host-vpc"  # Specify the name of your existing VPC
+    Name = "Jumphost-vpc"  # Specify the name of your existing VPC
   }
 }
 
@@ -121,7 +121,7 @@ data "aws_subnet" "subnet-1" {
  vpc_id = data.aws_vpc.main.id
  filter {
     name = "tag:Name"
-    values = ["subnet1"]
+    values = ["Jumphost-subnet1"]
  }
 }
 
@@ -129,14 +129,14 @@ data "aws_subnet" "subnet-2" {
  vpc_id = data.aws_vpc.main.id
  filter {
     name = "tag:Name"
-    values = ["subnet2"]
+    values = ["Jumphost-subnet2"]
  }
 }
 data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
   filter {
     name = "tag:Name"
-    values = ["jump-host-sg"]
+    values = ["jumphost-sg"]
  }
 }
 
