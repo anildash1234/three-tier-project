d34185c2535b0d43442d4a9032e166cf9c6057a6
anildash1234 <168274843+anildash1234@users.noreply.github.com>
2024-09-13 12:41:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,10 +6,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "state-remote-store-host"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+   backend "s3" {
+    bucket         = "cloudok098765"
+    key            = "vpc/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock-dynamo"
   }
 
   required_version = ">= 1.6.3"
